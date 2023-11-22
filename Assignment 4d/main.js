b2a536b229f4692c508704c859a21c8c3f3e1078
Simran241103 <144747608+Simran241103@users.noreply.github.com>
2023-11-22 18:01:53 -0500

    final
    Almost done

--- Assignment 4d/main.js
+++ Assignment 4d/main.js
@@ -1,4 +1,9 @@
-// set up canvas
+// Thanks to Renan Martineli for this version of the demo
+
+// setup canvas
+
+const para = document.querySelector('p');
+let count = 0;
 
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
@@ -6,102 +11,146 @@ const ctx = canvas.getContext('2d');
 const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
 
-// function to generate random number
-
+// Function to generate a random number within a range
 function random(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-// function to generate random RGB color value
-
-function randomRGB() {
-  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
-}
-
-class Ball {
-
-   constructor(x, y, velX, velY, color, size) {
-      this.x = x;
-      this.y = y;
-      this.velX = velX;
-      this.velY = velY;
-      this.color = color;
-      this.size = size;
+   return Math.floor(Math.random() * (max - min)) + min;
+ }
+ 
+ // Function to generate random RGB color value
+ function randomRGB() {
+   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+ }
+ 
+ // Define the Ball class
+ class Ball {
+   constructor(x, y, velX, velY, color, size, exists = true) {
+     this.x = x;
+     this.y = y;
+     this.velX = velX;
+     this.velY = velY;
+     this.color = color;
+     this.size = size;
+     this.exists = exists;
    }
-
-   draw() {
-      ctx.beginPath();
-      ctx.fillStyle = this.color;
-      ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-      ctx.fill();
+ 
+   draw(ctx) {
+     ctx.beginPath();
+     ctx.fillStyle = this.color;
+     ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+     ctx.fill();
    }
-
-   update() {
-      if ((this.x + this.size) >= width) {
-         this.velX = -(Math.abs(this.velX));
-      }
-
-      if ((this.x - this.size) <= 0) {
-         this.velX = Math.abs(this.velX);
-      }
-
-      if ((this.y + this.size) >= height) {
-         this.velY = -(Math.abs(this.velY));
-      }
-
-      if ((this.y - this.size) <= 0) {
-         this.velY = Math.abs(this.velY);
-      }
-
-      this.x += this.velX;
-      this.y += this.velY;
+ 
+   update(width, height) {
+     if ((this.x + this.size) >= width || (this.x - this.size) <= 0) {
+       this.velX = -this.velX;
+     }
+ 
+     if ((this.y + this.size) >= height || (this.y - this.size) <= 0) {
+       this.velY = -this.velY;
+     }
+ 
+     this.x += this.velX;
+     this.y += this.velY;
    }
-
-   collisionDetect() {
-      for (const ball of balls) {
-         if (!(this === ball)) {
-            const dx = this.x - ball.x;
-            const dy = this.y - ball.y;
-            const distance = Math.sqrt(dx * dx + dy * dy);
-
-            if (distance < this.size + ball.size) {
-              ball.color = this.color = randomRGB();
-            }
+ 
+   collisionDetect(balls) {
+     for (const ball of balls) {
+       if (this !== ball && ball.exists) {
+         const dx = this.x - ball.x;
+         const dy = this.y - ball.y;
+         const distance = Math.sqrt(dx * dx + dy * dy);
+ 
+         if (distance < this.size + ball.size) {
+           ball.color = this.color = randomRGB();
          }
-      }
+       }
+     }
    }
+ }
+ 
+ // Define the EvilCircle class
+ class EvilCircle {
+   constructor(x, y, size, color = 'white', velX = 20, velY = 20) {
+     this.x = x;
+     this.y = y;
+     this.size = size;
+     this.color = color;
+     this.velX = velX;
+     this.velY = velY;
+   }
+ 
+   draw(ctx) {
+     ctx.beginPath();
+     ctx.strokeStyle = this.color;
+     ctx.lineWidth = 3;
+     ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+     ctx.stroke();
+   }
+ 
+   checkBounds(width, height) {
+     if ((this.x + this.size) >= width || (this.x - this.size) <= 0) {
+       this.x -= this.velX;
+     }
+ 
+     if ((this.y + this.size) >= height || (this.y - this.size) <= 0) {
+       this.y -= this.velY;
+     }
+   }
+ 
+   collisionDetect(balls, para) {
+     for (const ball of balls) {
+       if (ball.exists) {
+         const dx = this.x - ball.x;
+         const dy = this.y - ball.y;
+         const distance = Math.sqrt(dx * dx + dy * dy);
+ 
+         if (distance < this.size + ball.size) {
+           ball.exists = false;
+           para.textContent = 'Ball count: ' + (--count);
+         }
+       }
+     }
+   }
+ }
 
-}
-
-const balls = [];
-
-while (balls.length < 25) {
-   const size = random(10,20);
+ // Define array to store balls and populate it
+ const balls = [];
+ 
+ while (balls.length < 25) {
+   const size = random(10, 20);
    const ball = new Ball(
-      // ball position always drawn at least one ball width
-      // away from the edge of the canvas, to avoid drawing errors
-      random(0 + size,width - size),
-      random(0 + size,height - size),
-      random(-7,7),
-      random(-7,7),
-      randomRGB(),
-      size
+     random(0 + size, width - size),
+     random(0 + size, height - size),
+     random(-7, 7),
+     random(-7, 7),
+     randomRGB(),
+     size
    );
-
-  balls.push(ball);
-}
-
-function loop() {
+   balls.push(ball);
+   count++;
+   para.textContent = 'Ball count: ' + count;
+ }
+ 
+ const evilBall = new EvilCircle(random(0, width), random(0, height), 10);
+ 
+ // The animation loop
+ function loop() {
    ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
-   ctx.fillRect(0, 0,  width, height);
-
+   ctx.fillRect(0, 0, width, height);
+ 
    for (const ball of balls) {
-     ball.draw();
-     ball.update();
-     ball.collisionDetect();
+     if (ball.exists) {
+       ball.draw(ctx);
+       ball.update(width, height);
+       ball.collisionDetect(balls);
+     }
    }
-
+ 
+   evilBall.draw(ctx);
+   evilBall.checkBounds(width, height);
+   evilBall.collisionDetect(balls, para);
+ 
    requestAnimationFrame(loop);
-}
-
-loop();
+ }
+ 
+ loop();
