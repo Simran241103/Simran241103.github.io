0891fd33c75a8c7588c07f1eb01ed83d14d3c969
Simran241103 <144747608+Simran241103@users.noreply.github.com>
2023-11-22 17:10:56 -0500

    final commit
    - everything is almost done

--- Assignment 4b/main.js
+++ Assignment 4b/main.js
@@ -1,18 +1,48 @@
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
-
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
-/* Declaring the array of image filenames */
+const imagesfilename = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
-/* Declaring the alternative text for each image file */
+const alttext = {
+  'pic1.jpg': 'Closeup of a human eye',
+  'pic2.jpg': 'Rock that looks like a wave',
+  'pic3.jpg': 'Purple and white pansies',
+  'pic4.jpg': "Section of wall from a pharaoh's tomb",
+  'pic5.jpg': 'Large moth on a leaf'
+};
 
-/* Looping through images */
+function createImageElement(imageSrc, altText) {
+  const newImage = document.createElement('img');
+  newImage.setAttribute('src', `images/${imageSrc}`);
+  newImage.setAttribute('alt', altText);
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+  newImage.addEventListener('click', e => {
+    displayedImage.src = e.target.src;
+    displayedImage.alt = e.target.alt;
+  });
 
-/* Wiring up the Darken/Lighten button */
+  return newImage;
+}
+
+function changeOverlay() {
+  const btnClass = btn.getAttribute('class');
+  if (btnClass === 'dark') {
+    btn.setAttribute('class', 'light');
+    btn.textContent = 'Lighten';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+  } else {
+    btn.setAttribute('class', 'dark');
+    btn.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+  }
+}
+
+for (const image of imagesfilename) {
+  const alt = alttext[image];
+  const newImage = createImageElement(image, alt);
+  thumbBar.appendChild(newImage);
+}
+
+btn.addEventListener('click', changeOverlay);
